1955ce4eeb4675cef58dcd47e457130087961202
Rishav Mandal <rishav@redappletech.com>
2025-03-20 16:46:50 +0530

    add single file todo checks

---
+++ README.md
@@ -0,0 +1,47 @@
+# TODO checker in files
+
+### Example
+
+> **NOTE** You must use **TODO:** in your comments.
+> Multiline todo comment is not supported yet.
+
+- Suppose you have a file like this:
+
+```go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Add a detailed about page content
+	fmt.Fprintf(w, "This is the about page.")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	// TODO: Implement a 404 page for unknown routes
+
+	fmt.Println("Server starting on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+```
+
+- Just run the binary `./todos-left ./src/main.go` and it will output:
+
+```text
+2 todos left in file "main.go":
+
+line 13: Add a detailed about page content
+line 21: Implement a 404 page for unknown routes
+```
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::{
-    env,
-    fs::{self},
-};
+use std::{env, fs, path::Path};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let f = &args[0];
 
-    match fs::read(args[0].to_string()) {
-        Ok(read_dir) => read_file(read_dir, f.to_string()),
-        Err(_) => println!("Could not read file `{}`. Does this file exist?", &f),
+    if args.is_empty() {
+        println!("Please provide a path to a file or directory");
+        return;
+    }
+
+    let path = Path::new(&args[0]);
+
+    if !path.exists() {
+        println!("The path is not a directory or does not exist");
+        return;
+    }
+
+    if path.is_file() {
+        read1_file(path);
+    } else if path.is_dir() {
+        todo!("read a directory recursively")
+    } else {
+        println!("Could not read this dir or file");
+        return;
     }
 }
 
-fn read_file(data: Vec<u8>, filename: String) {
+fn read1_file(path: &Path) {
+    let data = fs::read(path).expect("Cannot read file");
+
+    if let Some(os_str) = path.file_name() {
+        let filename = os_str
+            .to_str()
+            .map(|s| s.to_string())
+            .expect("UNKNOWN FILE");
+        check_todos(data, filename);
+    }
+}
+
+fn check_todos(data: Vec<u8>, filename: String) {
     let mut todos: Vec<String> = vec![];
     match String::from_utf8(data) {
         Ok(s) => {
@@ -22,7 +46,11 @@ fn read_file(data: Vec<u8>, filename: String) {
                     let todo_index = line.find("TODO:");
                     match todo_index {
                         Some(index) => {
-                            let str = format!("line {}: {}", line_no + 1, &line[index + 6..]);
+                            let str = format!(
+                                "line {}: {}",
+                                line_no + 1,
+                                &line[index + &"TODO:".len()..].trim_start()
+                            );
                             todos.push(str)
                         }
                         None => println!("{}", &line.trim_start()),
@@ -33,12 +61,13 @@ fn read_file(data: Vec<u8>, filename: String) {
         Err(e) => {
             println!("Oops some error while reading a file");
             println!("{}", e.to_string());
+            return;
         }
     };
     if todos.is_empty() {
         println!("No todos left. Great work!");
     } else {
-        println!("{} todos left in file `{}`", todos.len(), filename);
+        println!("{} todos left in file \"{}\":\n", todos.len(), filename);
         for todo in todos {
             println!("{}", todo);
         }
